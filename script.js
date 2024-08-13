c75dfec6ebc1838e317624bfaa65de14983eb7c6
wwwdaniju <130002882+wwwdaniju@users.noreply.github.com>
2024-08-13 19:23:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-onst caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
